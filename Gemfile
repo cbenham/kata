eff857aba60b0fcb3d9a408d2bd6a42e5657d1ed
Conrad Benham <cbenham@gmail.com>
2015-05-31 23:03:55 -0400

    Requiring all files in the lib directory.

--- Gemfile
+++ Gemfile
@@ -1,1 +1,2 @@
 gem 'rspec', require: 'spec'
+gem 'mocha'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,9 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+Dir['lib/**/*.rb'].each {|file| require file }
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -33,12 +36,7 @@ RSpec.configure do |config|
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
-  config.mock_with :rspec do |mocks|
-    # Prevents you from mocking or stubbing a method that does not exist on
-    # a real object. This is generally recommended, and will default to
-    # `true` in RSpec 4.
-    mocks.verify_partial_doubles = true
-  end
+  config.mock_with :mocha
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
